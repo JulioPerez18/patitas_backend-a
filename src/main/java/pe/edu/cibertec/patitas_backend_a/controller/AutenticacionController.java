c92c4197718507709f87b44b38fba299067e0357
JulioPerez18 <juliorubenperez14@gmail.com>
2024-10-20 22:19:28 -0500

    Commit CL3

--- src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
@@ -1,16 +1,24 @@
 package pe.edu.cibertec.patitas_backend_a.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.patitas_backend_a.dto.LogOutResponseDTO;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_backend_a.service.AutenticacionService;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 
 @RestController
@@ -19,6 +27,9 @@ public class AutenticacionController {
 
     @Autowired
     AutenticacionService autenticacionService;
+
+    @Value("${logout.file.path}")
+    private String filePath;
 
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
@@ -35,7 +46,23 @@ public class AutenticacionController {
         } catch (Exception e) {
             return new LoginResponseDTO("99", "Error: Ocurrió un problema", "", "");
         }
-
     }
+    @PostMapping("/logout")
+    public LogOutResponseDTO cerrarSesion(@RequestBody LoginRequestDTO loginRequestDTO) throws IOException {
+        // validar campos de entrada
+        if(loginRequestDTO.tipoDocumento() == null || loginRequestDTO.tipoDocumento().trim().length() == 0 ||
+                loginRequestDTO.numeroDocumento() == null || loginRequestDTO.numeroDocumento().trim().length() == 0) {
+            return new LogOutResponseDTO("01", "Error: No se encuentran las creedenciales");
+        }
 
+        String data = loginRequestDTO.tipoDocumento() + ";" + loginRequestDTO.numeroDocumento() + ";" + LocalDate.now().toString() + ";" + LocalTime.now().toString();
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            writer.write(data);
+            writer.newLine();
+            return new LogOutResponseDTO("00", "Datos guardados correctamente");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return new LogOutResponseDTO("02", "Error: No se pudo guardar en el txt");
+        }
+    }
 }
